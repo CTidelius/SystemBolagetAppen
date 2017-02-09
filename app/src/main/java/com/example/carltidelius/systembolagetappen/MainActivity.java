7b54b7b694cc1679f82debb469d90777dfa8c823
Carl Tidelius <carl_tidelius@live.se>
2017-02-09 20:24:19 +0100

    Addade arrayAdapter som en dummy för att testa

--- app/src/main/java/com/example/carltidelius/systembolagetappen/MainActivity.java
+++ app/src/main/java/com/example/carltidelius/systembolagetappen/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.carltidelius.systembolagetappen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.listview);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
+
+        arrayAdapter.add("bajs");
+
+
     }
 }
